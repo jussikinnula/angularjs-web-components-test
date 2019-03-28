4353f5a224b0540bcb23e6a063cdcfc99142a105
Jussi Kinnula <jussi.kinnula@efecte.com>
2019-03-28 12:12:07 +0200

    Add binding to normal web component

--- src/test-web-component.ts
+++ src/test-web-component.ts
@@ -1,9 +1,39 @@
 import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
+import { Observable, Subject } from 'rxjs';
 
 class TestWebComponent extends HTMLElement {
+  inputObservable: Observable<number>;
+  outputSubject: Subject<void>;
+  outputObservable: Observable<void>;
+  textElement: HTMLElement;
+
   constructor() {
     super();
-    this.innerHTML = 'This is a test web component';
+
+    this.outputSubject = new Subject();
+    this.outputObservable = this.outputSubject.asObservable();
+
+    this.textElement = document.createElement('span');
+    this.textElement.innerHTML = 'Test web component initialized';
+    this.appendChild(this.textElement);
+
+    const button = document.createElement('button');
+    button.innerHTML = 'Increment';
+    button.onclick = this.clickHandler.bind(this);
+    button.style.marginLeft = '0.5em';
+    this.appendChild(button);
+
+    setTimeout(() => this.setup());
+  }
+
+  clickHandler() {
+    this.outputSubject.next();
+  }
+
+  setup() {
+    this.inputObservable.subscribe(value => {
+      this.textElement.innerHTML = `This is a test web component with value: <b>${value.toString()}</b>`;
+    });
   }
 }
 
